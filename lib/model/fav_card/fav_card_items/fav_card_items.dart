96f4b8d55d788001097bd54aa27d49f9601892c0
Uvaish Zafri <uvaishzafri1@gmail.com>
2023-11-16 00:30:42 +0530

    fav card models

---
+++ lib/model/fav_card/fav_card_items/fav_card_items.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'fav_card_items.freezed.dart';
+
+@freezed
+class FavCardItemModel with _$FavCardItemModel {
+  const factory FavCardItemModel({
+    required String id,
+    required String name,
+    required List<String> categories,
+    required Image image,
+  }) = _FavCardItemModel;
+}
